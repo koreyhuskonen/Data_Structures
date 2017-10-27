809f68f4f1559a15dff4f1858d018ffe7370ee0a
Korey Huskonen <huskonkw@mail.uc.edu>
2017-10-26 20:55:35 -0400

    Add HeapSort

--- HW/HW2/hw2_KoreyHuskonen.cpp
+++ HW/HW2/hw2_KoreyHuskonen.cpp
@@ -18,14 +18,15 @@ int main(){
          << left << setw(w) << "Quick"
          << left << setw(w) << "Count"
          << left << setw(w) << "Radix"
+         << left << setw(w) << "Heap"
          << endl;
 
-    int num_algos = 7, num_trials = 10, sizes[5] = {10,100,500,5000,25000};
+    int num_algos = 8, num_trials = 10, sizes[5] = {10,100,500,5000,25000};
     unsigned long long bubble_sum, insertion_sum, selection_sum, merge_sum, quick_sum,
-                       count_sum, radix_sum;
+                       count_sum, radix_sum, heap_sum;
     for(int x = 0; x < 5; x++){
         int size = sizes[x];
-        bubble_sum = insertion_sum = selection_sum = merge_sum = quick_sum = count_sum = radix_sum = 0;
+        bubble_sum = insertion_sum = selection_sum = merge_sum = quick_sum = count_sum = radix_sum = heap_sum = 0;
         for(int j = 0; j < num_trials; j++){
             int test[num_algos][size];
             for(int i = 0; i < size; i++){
@@ -77,6 +78,11 @@ int main(){
             t2 = Clock::now();
             radix_sum += chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count();
 
+            t1 = Clock::now();
+            heapSort(test[7], size);
+            t2 = Clock::now();
+            heap_sum += chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count();
+
             // for(int j = 0; j < num_algos; j++){
             //     for(int i = 0; i < size; i++){
             //         cout << test[j][i] << endl;
@@ -93,96 +99,9 @@ int main(){
              << left << setw(w) << quick_sum/num_trials
              << left << setw(w) << count_sum/num_trials
              << left << setw(w) << radix_sum/num_trials
+             << left << setw(w) << heap_sum/num_trials
              << endl;
 
     }
 
-
-
-
-
-
-
-
-
-
-    // int num_algos = 5, num_tests = 3, test_sizes[] = {10, 100, 500};
-    // int* test[num_algos][num_tests];
-    // // *test* will store all the test cases indexed by algorithm and then by case number
-    // // We must intialize each test case to the proper size, stored in the *test_sizes* array
-    // for(int i = 0; i < num_algos; i++){
-    //     for(int j = 0; j < num_tests; j++){
-    //         test[i][j] = new int[test_sizes[j]];
-    //     }
-    // }
-    // // Populate each test case
-    // srand(time(0));
-    // for(int i = 0; i < num_tests; i++){              // Select each test case
-    //     for(int j = 0; j < test_sizes[i]; j++){      // Fill test case
-    //         int temp = rand() % (2*test_sizes[i]+1); // Fill with integers from 0 to 2n, where n is the size contained in *test_sizes*
-    //         for(int k = 0; k < num_algos; k++){      // Use the same integer for each algorithm (the first index of *test*)
-    //             test[k][i][j] = temp;
-    //         }
-    //     }
-    // }
-    // // Display table header
-    // int w = 15; // *w* = width of each table column
-    // cout << setw(w) << "";
-    // for(int i = 0; i < num_tests; i++){
-    //     cout << left << setw(w) << test_sizes[i];
-    // }
-    // cout << endl;
-    //
-    // int num_trials = 10;
-    //
-    // cout << left << setw(w) << "Bubble";
-    // for(int )
-    // for(int i = 0; i < num_tests; i++){
-    //     auto t1 = Clock::now();
-    //     bubble(test[0][i], test_sizes[i]);
-    //     auto t2 = Clock::now();
-    //     cout << left << setw(w) << chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count();
-    // }
-    // cout << endl;
-    //
-    // cout << left << setw(w) << "Insertion";
-    // for(int i = 0; i < num_tests; i++){
-    //     auto t1 = Clock::now();
-    //     insertion(test[1][i], test_sizes[i]);
-    //     auto t2 = Clock::now();
-    //     cout << left << setw(w) << chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count();
-    // }
-    // cout << endl;
-    //
-    // cout << left << setw(w) << "Selection";
-    // for(int i = 0; i < num_tests; i++){
-    //     auto t1 = Clock::now();
-    //     selection(test[2][i], test_sizes[i]);
-    //     auto t2 = Clock::now();
-    //     cout << left << setw(w) << chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count();
-    // }
-    // cout << endl;
-    //
-    // cout << left << setw(w) << "Merge";
-    // for(int i = 0; i < num_tests; i++){
-    //     auto t1 = Clock::now();
-    //     mergeSort(test[3][i], 0, test_sizes[i]-1);
-    //     auto t2 = Clock::now();
-    //     cout << left << setw(w) << chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count();
-    // }
-    // cout << endl;
-    //
-    // cout << left << setw(w) << "Quick";
-    // for(int i = 0; i < num_tests; i++){
-    //     auto t1 = Clock::now();
-    //     quickSort(test[4][i], 0, test_sizes[i]-1);
-    //     auto t2 = Clock::now();
-    //     cout << left << setw(w) << chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count();
-    // }
-    // cout << endl;
-    // for(int i = 0; i < num_algos; i++){
-    //     for(int j = 0; j < num_tests; j++){
-    //         delete [] test[i][j];
-    //     }
-    // }
 }
--- HW/HW2/sorting.cpp
+++ HW/HW2/sorting.cpp
@@ -122,8 +122,10 @@ void quickSort(int* arr, int low, int high){
 
 void countSort(int* arr, int size){
     int range = *max_element(arr, arr+size) + 1; // +1 to include last index
-    int c[range], output[size];
-    memset(c, 0, sizeof(c));
+    // int c[range], output[size];
+    int output[size], *c = new int[range];
+    // memset(c, 0, sizeof(c));
+    for(int i = 0; i < range; i++) c[i] = 0;
     for(int i = 0; i < size; i++){
         c[arr[i]]++;
     }
@@ -137,6 +139,8 @@ void countSort(int* arr, int size){
     for(int i = 0; i < size; i++){
         arr[i] = output[i];
     }
+
+    delete [] c;
 }
 
 void radix(int* arr, int size){
@@ -144,12 +148,12 @@ void radix(int* arr, int size){
     int output[size];
 
     for(int j = 1; max_val/j > 0; j *= 10){
+
         int c[10] = {0};
         for(int i = 0; i < size; i++){
             int index = arr[i]/j % 10;
             c[index]++;
         }
-
         for(int i = 1; i < 10; i++){
             c[i] += c[i-1];
         }
@@ -165,17 +169,34 @@ void radix(int* arr, int size){
     }
 }
 
-// int main(){
-//     int bob[6] = {1,2,44,3,77799999,12345};
-//     radix(bob, 6);
-//     for(int i = 0; i < 6; i++) cout << bob[i] << endl;
-// }
+void heapify(int* arr, int n, int i){
+    int largest = i;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+    if(l < n && arr[l] > arr[largest]) largest = l;
+    if(r < n && arr[r] > arr[largest]) largest = r;
+    if(largest != i){
+        swap(arr, i, largest);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int* arr, int size){
+    for(int i = size / 2 - 1; i >= 0; i--){
+        heapify(arr, size, i);
+    }
+
+    for(int i = size - 1; i >= 0; i--){
+        swap(arr, 0, i);
+        heapify(arr, i, 0);
+    }
+}
 
 // int main(){
 //     struct timespec start, finish;
 //     double elapsed;
 //     srand(time(NULL));
-//     int size = 15, num_tests = 7, test_input[num_tests][size], temp;
+//     int size = 15, num_tests = 8, test_input[num_tests][size], temp;
 //     for(int j = 0; j < size; j++){
 //         temp = rand() % 100;
 //         for(int i = 0; i < num_tests; i++){
@@ -185,7 +206,7 @@ void radix(int* arr, int size){
 //
 //     cout << "\n_____ Unsorted _____" << endl;
 //     for(int i = 0; i < size; i++) cout << test_input[0][i] << endl;
-//
+
 // //     cout << "\nBubble" << endl;
 // //     clock_gettime(CLOCK_MONOTONIC, &start);
 // //     bubble(test_input[0], size);
@@ -248,4 +269,13 @@ void radix(int* arr, int size){
 //     elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
 //     for(int i = 0; i < size; i++) cout << test_input[6][i] << endl;
 //     cout << "Time: " << elapsed << endl;
+    //
+    // cout << "\nHeapSort" << endl;
+    // clock_gettime(CLOCK_MONOTONIC, &start);
+    // heapSort(test_input[7], size);
+    // clock_gettime(CLOCK_MONOTONIC, &finish);
+    // elapsed = (finish.tv_sec - start.tv_sec);
+    // elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+    // for(int i = 0; i < size; i++) cout << test_input[7][i] << endl;
+    // cout << "Time: " << elapsed << endl;
 // }
